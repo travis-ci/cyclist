6c1511fe1f9a2a69fc39ac2169be4af99f150646
Dan Buch <daniel.buch@gmail.com>
2016-09-16 21:52:46 -0400

    Tweaks to SNS verification plus more logging

--- sns_message.go
+++ sns_message.go
@@ -2,23 +2,27 @@ package cyclist
 
 import (
 	"bytes"
-	"crypto"
-	"crypto/rsa"
-	"crypto/sha1"
 	"crypto/x509"
 	"encoding/base64"
 	"encoding/json"
 	"encoding/pem"
-	"fmt"
 	"io/ioutil"
 	"net/http"
 	"reflect"
+
+	"github.com/pkg/errors"
 )
 
 var (
 	// NOTE: this declaration is broken up mostly to appease the gofmt checker,
 	//       although I don't know why ~meatballhat
-	snsSigKeys = map[string][]string{}
+	snsSigKeys      = map[string][]string{}
+	snsKeyRealNames = map[string]string{
+		"MessageID": "MessageId",
+		"TopicARN":  "TopicArn",
+	}
+
+	errEmptyPEM = errors.New("nothing found in pem encoded bytes")
 )
 
 func init() {
@@ -63,59 +67,57 @@ func (m *snsMessage) lifecycleAction() (*lifecycleAction, error) {
 	return a, nil
 }
 
-func (m *snsMessage) verify() error {
+func (m *snsMessage) sigSerialized() []byte {
 	buf := &bytes.Buffer{}
 	v := reflect.ValueOf(m)
 
 	for _, key := range snsSigKeys[m.Type] {
-		val := reflect.Indirect(v).FieldByName(key).String()
-		if val == "" {
+		field := reflect.Indirect(v).FieldByName(key)
+		val := field.String()
+		if !field.IsValid() || val == "" {
 			continue
+		}
+		if rn, ok := snsKeyRealNames[key]; ok {
+			key = rn
 		}
 		buf.WriteString(key + "\n")
 		buf.WriteString(val + "\n")
 	}
 
+	return buf.Bytes()
+}
+
+func (m *snsMessage) verify() error {
 	msgSig, err := base64.StdEncoding.DecodeString(m.Signature)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to base64 decode signature")
 	}
 
 	res, err := http.Get(m.SigningCertURL)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to fetch signing cert")
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to read signing cert body")
 	}
 
 	p, _ := pem.Decode(body)
+	if p == nil {
+		return errEmptyPEM
+	}
+
 	cert, err := x509.ParseCertificate(p.Bytes)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "failed to parse signing cert")
 	}
 
-	err = cert.CheckSignature(x509.SHA1WithRSA, buf.Bytes(), msgSig)
+	err = cert.CheckSignature(x509.SHA1WithRSA, m.sigSerialized(), msgSig)
 	if err != nil {
-		return err
+		return errors.Wrap(err, "message signature check error")
 	}
 
-	pub, ok := cert.PublicKey.(*rsa.PublicKey)
-	if !ok {
-		return fmt.Errorf("unknown public key type %T", cert.PublicKey)
-	}
-
-	h := sha1.New()
-	n, err := h.Write(buf.Bytes())
-	if err != nil {
-		return err
-	}
-	if n != buf.Len() {
-		return fmt.Errorf("unexpected number of bytes written expected=%d actual=%d", buf.Len(), n)
-	}
-
-	return rsa.VerifyPKCS1v15(pub, crypto.SHA1, h.Sum(nil), msgSig)
+	return nil
 }
